f46c370a57d244f4b345811227fd67bc771d896f
Gulin Sergey <info@wb-group.ru>
2019-04-03 22:09:45 +0300

    66page

---
+++ func_param.py
@@ -0,0 +1,8 @@
+def printMax(a, b):
+    if a > b:
+        print(a, 'максимально')
+    elif a == b:
+        print(a, 'равно', b)
+    else:
+        print(b, 'максимально')
+printMax(3, 4) # прямая передача значений
